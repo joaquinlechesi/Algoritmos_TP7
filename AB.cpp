3885f0f28c5fe5da90d1fdcbf61f76c55b498ec5
joaquinlechesi <joaquinlechesi@gmail.com>
2026-05-25 00:34:13 -0300

    Comenzando programa de prueba

--- AB.cpp
+++ AB.cpp
@@ -2,6 +2,23 @@
 
 int main()
 {
-    /* code */
+    puts("ADT AB");
+    puts("Defino un nuevo arbol binario 'T1'");
+    AB T1;
+    T1 = ABVACIO();
+    puts("Es 'T1' arbol vacio?");
+    if (ESABVACIO(T1))
+    {
+        puts("Si.");
+    }
+    else
+    {
+        puts("No.");
+    }
+    
+    
+    puts("Presione la tecla enter para finalizar.");
+    fflush(stdin);
+    getchar();
     return 0;
 }
