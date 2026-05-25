90e0bfe8be9e440f0553c535a6ff9529ade2af6f
joaquinlechesi <joaquinlechesi@gmail.com>
2026-05-25 00:48:28 -0300

    Probando la operacion ARMARAB

--- AB.cpp
+++ AB.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     puts("ADT AB");
-    puts("Defino un nuevo arbol binario 'T1'");
+    puts("Defino un nuevo arbol binario vacio 'T1'");
     AB T1;
     T1 = ABVACIO();
     puts("Es 'T1' arbol vacio?");
@@ -15,9 +15,24 @@ int main()
     {
         puts("No.");
     }
+    puts("Defino dos arboles AB, uno llamado 'I' y otro llamado 'D'. Tambien defino un nuevo itemAB 'r' con el valor 'a'");
+    AB I = ABVACIO();
+    AB D = ABVACIO();
+    itemAB r = 'a';
+    puts("Agrego ambos arboles y el item a 'T1'");
+    puts("'T1 = ARMARAB(I, r, D)'");
+    T1 = ARMARAB(I, r, D);
+    puts("Es 'T1' arbol vacio?");
+    if (ESABVACIO(T1))
+    {
+        puts("Si.");
+    }
+    else
+    {
+        puts("No.");
+    }
     
-    
-    puts("Presione la tecla enter para finalizar.");
+    puts("\nPresione la tecla enter para finalizar.");
     fflush(stdin);
     getchar();
     return 0;
